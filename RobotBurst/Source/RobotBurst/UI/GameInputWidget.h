efd3f71d93810570c79d551378a92f1a9bf2b0d2
am <zhoulixi1988@aliyun.com>
2018-01-11 10:27:11 +0800

    4

---
+++ RobotBurst/Source/RobotBurst/UI/GameInputWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/BaseUserWidget.h"
+#include "GameInputWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTBURST_API UGameInputWidget : public UBaseUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
