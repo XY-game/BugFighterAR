9b503e3514459b6aec7f84c33fb84420dba9b53a
am <zhoulixi1988@aliyun.com>
2018-01-11 10:24:42 +0800

    2

---
+++ RobotBurst/Source/RobotBurst/UI/PlayerTouchInterface.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/TouchInterface.h"
+#include "PlayerTouchInterface.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTBURST_API UPlayerTouchInterface : public UTouchInterface
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
